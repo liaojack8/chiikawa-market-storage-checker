571504352b6dd488e7758a0817192b3c18b3b0a0
EiC <liaojack8.tw@gmail.com>
2024-10-25 21:55:26 +0800

    Update chiikawa-market-storage-checker.user.js

--- chiikawa-market-storage-checker.user.js
+++ chiikawa-market-storage-checker.user.js
@@ -1,17 +1,19 @@
 // ==UserScript==
 // @name         Chiikawa Market Storage Checker
-// @namespace    https://github.com/zhxie/chiikawa-market-storage-checker
-// @version      2024-06-28
-// @author       Xie Zhihao
+// @namespace    https://github.com/liaojack8
+// @version      2024-10-25
+// @author       liaojack8
 // @description  Check storage of products in Chiikawa market.
-// @homepage     https://github.com/zhxie/chiikawa-market-storage-checker
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=chiikawamarket.jp
+// @homepage     https://github.com/liaojack8/chiikawa-market-storage-checker
+// @icon         https://chiikawamarket.jp/cdn/shop/files/fav.png
 // @match        https://chiikawamarket.jp/products/*
 // @match        https://chiikawamarket.jp/collections/*/products/*
 // @match        https://chiikawamarket.jp/cart
 // @match        https://nagano-market.jp/products/*
 // @match        https://nagano-market.jp/collections/*/products/*
 // @match        https://nagano-market.jp/cart
+// @downloadURL       https://raw.githubusercontent.com/liaojack8/chiikawa-market-storage-checker/refs/heads/master/chiikawa-market-storage-checker.user.js#
+// @updateURL       https://raw.githubusercontent.com/liaojack8/chiikawa-market-storage-checker/refs/heads/master/chiikawa-market-storage-checker.user.js#
 // @grant        none
 // ==/UserScript==
 
