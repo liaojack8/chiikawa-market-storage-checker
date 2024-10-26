1b2bc8775c947cb13d50180926a7b49783f80a6c
EiC <liaojack8.tw@gmail.com>
2024-10-26 22:34:05 +0800

    Update chiikawa-market-storage-checker.user.js

--- chiikawa-market-storage-checker.user.js
+++ chiikawa-market-storage-checker.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Chiikawa Market Storage Checker
 // @namespace    https://github.com/liaojack8
-// @version      2024-10-25
+// @version      v20241026.1
 // @author       liaojack8
 // @description  Check storage of products in Chiikawa market.
 // @homepage     https://github.com/liaojack8/chiikawa-market-storage-checker
@@ -12,34 +12,37 @@
 // @match        https://nagano-market.jp/products/*
 // @match        https://nagano-market.jp/collections/*/products/*
 // @match        https://nagano-market.jp/cart
-// @downloadURL       https://raw.githubusercontent.com/liaojack8/chiikawa-market-storage-checker/refs/heads/master/chiikawa-market-storage-checker.user.js#
-// @updateURL       https://raw.githubusercontent.com/liaojack8/chiikawa-market-storage-checker/refs/heads/master/chiikawa-market-storage-checker.user.js#
+// @downloadURL  https://raw.githubusercontent.com/liaojack8/chiikawa-market-storage-checker/refs/heads/master/chiikawa-market-storage-checker.user.js#
+// @updateURL    https://raw.githubusercontent.com/liaojack8/chiikawa-market-storage-checker/refs/heads/master/chiikawa-market-storage-checker.user.js#
 // @grant        none
 // ==/UserScript==
 
 (function () {
-  "use strict";
-  // remove worldshopping
-  document.querySelector('#zigzag-worldshopping-checkout').remove();
-  if (document.location.pathname === "/cart") {
-    // Cart.
-    for (const item of document.getElementsByClassName("cart--item")) {
-      const quantity = item.getAttribute("data-inventory-quantity");
-      const label =
-        item.getElementsByClassName("cart--item--title")?.[0]?.children?.[0]
-          ?.children?.[0];
-      if (quantity !== undefined && label) {
-        label.textContent += ` [庫存: ${quantity}]`;
-      }
+    "use strict";
+    function fuckWorldshopping() {
+        // remove worldshopping
+        document.querySelector('#zigzag-worldshopping-checkout').remove();
     }
-  } else {
-    // Product.
-    const quantity = document
-      .getElementsByClassName("product-form--variant-select")?.[0]
-      ?.children?.[0]?.getAttribute("data-inventory-quantity");
-    const label = document.getElementsByClassName("product-page--title")?.[0];
-    if (quantity !== undefined && label) {
-      label.textContent += ` [庫存: ${quantity}]`;
+    if (document.location.pathname === "/cart") {
+        // Cart.
+        for (const item of document.getElementsByClassName("cart--item")) {
+            const quantity = item.getAttribute("data-inventory-quantity");
+            const label =
+                item.getElementsByClassName("cart--item--title")?.[0]?.children?.[0]
+                    ?.children?.[0];
+            if (quantity !== undefined && label) {
+                label.textContent += ` [庫存: ${quantity}]`;
+            }
+        }
+    } else {
+        // Product.
+        const quantity = document
+            .getElementsByClassName("product-form--variant-select")?.[0]
+            ?.children?.[0]?.getAttribute("data-inventory-quantity");
+        const label = document.getElementsByClassName("product-page--title")?.[0];
+        if (quantity !== undefined && label) {
+            label.textContent += ` [庫存: ${quantity}]`;
+        }
     }
-  }
+    window.addEventListener('load', setTimeout(fuckWorldshopping, 2000));
 })();
